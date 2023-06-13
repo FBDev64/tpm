b224145bb8b643ff6b3b207a39a14d33196fad79
EvrestRGB <elzadam11@gmail.com>
2023-06-13 18:45:45 +0100

    Update module.php
    I added the escapeHTML()

--- module/module.php
+++ module/module.php
@@ -12,6 +12,11 @@ function assignVariable($variable, $value) {
     $$variable = $value;
 }
 
+// Fonction pour échapper les caractères spéciaux pour prévenir les injections XSS
+function escapeHtml($data) {
+    return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
+}
+
 // Exécution du code
 function executeCode($code) {
     eval($code);
@@ -22,7 +27,13 @@ $code = '<?tpm
 $var1 = 5;
 $var2 = 10;
 $result = $var1 + $var2;
+
+$userInput = "<script>alert(\'XSS\');</script>";
+$escapedInput = escapeHtml($userInput);
+
 echo "Le résultat est : " . $result;
+echo "<br>";
+echo "Entrée utilisateur échappée : " . $escapedInput;
 ?>';
 
 // Exécution du code
@@ -30,5 +41,6 @@ executeCode($code);
 
 // Résultat :
 // Le résultat est : 15
+// Entrée utilisateur échappée : &lt;script&gt;alert(&#039;XSS&#039;);&lt;/script&gt;
 
 ?>
